95e4d7275a06f4803db954f29c0061825009621f
Neal Gompa <ngompa13@gmail.com>
2019-03-16 16:42:18 -0400

    Restart systemd-binfmt so that qemu-user-static works

--- mkimage-dnf.sh
+++ mkimage-dnf.sh
@@ -76,6 +76,8 @@ if [ ! -z $buildarch ]; then
 			echo "Error: 'qemu-user-static' needs to be installed for non-native rootfs builds!"
 			exit 1
 		fi
+		# To ensure qemu-user-static can be used, restart systemd-binfmt
+		systemctl restart systemd-binfmt.service
 	fi
 fi
 
