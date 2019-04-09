5bd2cabdefa89b2af738d45a790084432449bdb2
Neal Gompa <ngompa13@gmail.com>
2019-04-08 21:36:12 -0400

    mkimage.sh: If urpmi is requested, configure media from within chroot
    Sometimes, there's a bit of a mismatch between the host's urpmi
    and the guest environment, such that it's unable to correctly add
    the media and generate the urpmi.cfg. For example, it is not possible
    to configure the rootfs urpmi.cfg for aarch64 from Mageia 6's urpmi.
    
    In order to avoid that problem, we simply always execute urpmi.addmedia
    from within the chroot. This gives us the side benefit of removing
    the requirement for urpmi on the host when urpmi is going to be
    installed into the rootfs.

--- mkimage.sh
+++ mkimage.sh
@@ -126,11 +126,18 @@ if [ -z $pkgmgr ]; then
         pkgmgr="dnf urpmi"
 fi
 
+extrapkgs=""
+# If urpmi is included, we *must* add curl to the target environment,
+# since aria2 segfaults on some architectures, like aarch64
+if [[ $pkgmgr == *"urpmi"* ]]; then
+        extrapkgs="$extrapkgs curl"
+fi
+
 if [ ! -z $systemd ]; then
         echo -e "--------------------------------------"
         echo -e "Creating image with systemd support."
         echo -e "--------------------------------------\n"
-        systemd="systemd"
+        extrapkgs="$extrapkgs systemd"
 fi
 
 (
@@ -141,15 +148,23 @@ fi
             --releasever="$releasever" \
             --setopt=install_weak_deps=False \
             --nodocs --assumeyes ${quiet: --quiet} \
-            install basesystem-minimal $pkgmgr locales locales-en $systemd
+            install basesystem-minimal $pkgmgr locales locales-en $extrapkgs
 )
 
+# Make sure /etc/resolv.conf has something useful in it
+# This is being done before urpmi.addmedia call to ensure
+# that will work from within the chroot...
+mkdir -p "$rootfsDir/etc"
+cat > "$rootfsDir/etc/resolv.conf" <<'EOF'
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+
 # Configure urpmi mirrorlist if urpmi is included on the system
+# We do this in within the root to minimize impact from host,
+# and so that target system architectures can be properly configured
 if [[ $pkgmgr == *"urpmi"* ]]; then
-        if [ -x /usr/sbin/urpmi.addmedia ]; then
-                urpmi.addmedia --distrib --mirrorlist "https://mirrors.mageia.org/api/mageia.$releasever.$buildarch.list" \
-                               --urpmi-root "$rootfsDir"
-        fi
+        chroot "$rootfsDir" urpmi.addmedia --curl --distrib --mirrorlist "https://mirrors.mageia.org/api/mageia.$releasever.$buildarch.list"
 fi
 
 "$(dirname "$BASH_SOURCE")/.febootstrap-minimize" "$rootfsDir"
@@ -175,13 +190,6 @@ fi
 # Docker mounts tmpfs at /dev and procfs at /proc so we can remove them
 rm -rf "$rootfsDir/dev" "$rootfsDir/proc"
 mkdir -p "$rootfsDir/dev" "$rootfsDir/proc"
-
-# make sure /etc/resolv.conf has something useful in it
-mkdir -p "$rootfsDir/etc"
-cat > "$rootfsDir/etc/resolv.conf" <<'EOF'
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOF
 
 if [ ! -z $systemd ]; then
     tarFile="$dir/rootfs-systemd.tar.xz"
